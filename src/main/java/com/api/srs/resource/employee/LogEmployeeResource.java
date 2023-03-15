8f53faaefddb39b9c5ffd7b781486cea0ca1c925
Gabriel <gabrielhpereira518@gmail.com>
2023-03-15 21:12:58 +0000

    Added listAllLogEmployee in controller

--- src/main/java/com/api/srs/resource/employee/LogEmployeeResource.java
+++ src/main/java/com/api/srs/resource/employee/LogEmployeeResource.java
@@ -2,8 +2,14 @@ package com.api.srs.resource.employee;
 
 import com.api.srs.service.employee.LogEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.ValidationException;
 
 @RestController
 @RequestMapping("/logEmployee")
@@ -11,4 +17,15 @@ public class LogEmployeeResource {
 
     @Autowired
     private LogEmployeeService logEmployeeService;
+
+    @GetMapping("/listAllLogEmployee/{employeeCpf}")
+    public ResponseEntity<Object> listAllLogEmployee(@PathVariable String employeeCpf) {
+        try {
+            return new ResponseEntity<>(this.logEmployeeService.listAllLogEmployee(employeeCpf), HttpStatus.OK);
+        } catch (ValidationException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
